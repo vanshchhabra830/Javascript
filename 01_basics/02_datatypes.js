588b80b334f5817701a310d8e0e68dbb1042a8d9
Vansh Chhabra <98265056+vanshchhabra830@users.noreply.github.com>
2024-08-08 17:14:59 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";  // will treat code as newer version of js
+
+// alert( 6 + 9 ) // we are using node js not browser
+
+let name = "Vansh"
+let age = 19
+let isloggedin = false
+let state;
+let subject = null
+
+
+// datatypes in JS//
+
+// string => ""
+// bigint
+// null => standalone value
+// number => from 2 to the power 53
+// undefined => value not defined
+// boolean => true/false
+// symbol => unique
+// object
+
+console.log(typeof "Vansh");
+console.log(typeof undefined); // undefined gives undefined
+console.log(typeof null); // null gives object
+
